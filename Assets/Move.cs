bcbd123f9c7a2f4d62d558179bd969559e6c10d2
elitheman <75771181+elitheman@users.noreply.github.com>
2022-07-28 13:36:33 -0600

    scene restarts

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -22,6 +22,10 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if(Input.GetButtonDown("Fire2")){
+            Application.LoadLevel(Application.loadedLevel);}
+
         if(Input.GetButtonDown("Fire1")){
             start = true;}
 
